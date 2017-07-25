4569518b57d989cc982244939e25dede81357102
hongzhouye <hzye2011@mit.edu>
2017-07-25 14:18:02 -0400

    Creat module fcm w/ +-*/

---
+++ fcm/math.py
@@ -0,0 +1,31 @@
+"""
+A small set of mathematical functions.
+"""
+
+# add: adds two arguments
+def add(arg1, arg2):
+    """
+    Function that adds two arguments.
+    """
+    return arg1 + arg2
+
+# sub: subtracts two arguments
+def sub(arg1, arg2):
+    """
+    Function that subtracts two arguments.
+    """
+    return arg1 - arg2
+
+# mult: multiplies two arguments
+def mult(arg1, arg2):
+    """
+    Function that multiplies two arguments.
+    """
+    return arg1 * arg2
+
+# quot: quotient of two arguments
+def quot(arg1, arg2):
+    """
+    Function that gives quotient of two arguments
+    """
+    return arg1 / arg2
